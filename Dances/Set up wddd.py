53dd288a229c7ee642d90c94e52a8dc5799abe9a
Foztarz <jjfoster86@gmail.com>
2021-05-06 12:20:36 +0200

    Trying out bb_wdd.

---
+++ Dances/Set up wddd.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+# Details ---------------------------------------------------------------
+#       AUTHOR:	James Foster        DATE: 2021 05 05
+#     MODIFIED:	James Foster        DATE: 2021 05 05
+#
+#  DESCRIPTION: Install dependencies for wdd2 from github.com/BioroboticsLab
+#               
+#      OUTPUTS: None.
+#
+#	   CHANGES: -
+#
+#   REFERENCES: Wario, F., Wild, B., Rojas, R., Landgraf, T., 2017.
+#               Automatic detection and decoding of honey bee waggle dances. 
+#               PLoS ONE 12, 1–16. 
+#               https://doi.org/10.1371/journal.pone.0188626
+
+# 
+#TODO   
+#- Check packages
+#- Test video  
+#- Test own video
+
+"""
+pip install click
+#conda install sys # not found
+#conda install time # not found
+pip install imageio
+#conda install distutils # not found
---
+++ Dances/webcam_view.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  6 09:09:55 2021
+https://stackoverflow.com/a/11449901/660921
+@author: jaf54iq
+"""
+import cv2
+cv2.namedWindow("preview")
+vc = cv2.VideoCapture(0)
+
+if vc.isOpened(): # try to get the first frame
+    rval, frame = vc.read()
+else:
+    rval = False
+
+while rval:
+    cv2.imshow("preview", frame)
+    rval, frame = vc.read()
+    key = cv2.waitKey(20)
+    if key == 27: # exit on ESC
+        break
+cv2.destroyWindow("preview")
