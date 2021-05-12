fc8fc272a1823e3756f63415b7a627e03c63640f
Foztarz <jjfoster86@gmail.com>
2021-05-12 17:39:36 +0200

    General setup

---
+++ Imaging/setup_opencv.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 10 10:12:52 2021
+
+@author: jaf54iq
+"""
+
+conda create --name ComputerVision python=2.7 -y
+conda activate ComputerVision
+#needs Spyder 3.3.6 to run at all. Deprecated 2020.01.01
+#conda create --name ComputerVision python=3.7 -y
+#conda list --revisions
+
+#in TERMINAL
+#source activate /Users/jamesfoster/opt/anaconda3/envs/ComputerVision
+
+#conda install -c menpo opencv -y#menpo opencv is not yet 3.8 compatible
+#conda install python=3.7#THIS WAS A VERY BAD IDEA
+
+# conda create --name ComputerVision python=3.7 -y
+#Open the environment in Anaconda Navigator, don't try it here
+#check which environment you're in
+conda info --envs
+conda install -c menpo opencv
+
+pip install opencv-python
+#conda install -c anaconda numpy -y
+conda install pandas -y
+conda install -c anaconda sqlalchemy -y
+#conda install -c conda- forge matplotlib -y
+conda install matplotlib
