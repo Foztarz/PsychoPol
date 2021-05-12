ea12b392eb391e51980b9446fd9dbb82d4a38892
Foztarz <jjfoster86@gmail.com>
2021-05-12 08:46:25 +0200

    External camera

--- Dances/webcam_view.py
+++ Dances/webcam_view.py
@@ -6,7 +6,7 @@ https://stackoverflow.com/a/11449901/660921
 """
 import cv2
 cv2.namedWindow("preview")
-vc = cv2.VideoCapture(0)
+vc = cv2.VideoCapture(1)#0)
 
 if vc.isOpened(): # try to get the first frame
     rval, frame = vc.read()
