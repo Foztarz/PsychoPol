a18ba326ee6f2ec3853236cdb8bff11ac1af52ac
Foztarz <jjfoster86@gmail.com>
2021-10-14 17:35:11 +0200

    Load invertsy

--- Model/testing_InvertPy.py
+++ Model/testing_InvertPy.py
@@ -51,7 +51,7 @@ sys.path
 """
 # Import InvertPy
 """
-conda info --envs
+conda info --envs 
 #if invertpy is not currently activated, activate it
         # conda activate invertpy
 #make sure that invpy is installed in this environment
@@ -82,6 +82,18 @@ os.listdir(inpy +'/examples')
         #  'run_incentive_circuit.py',
         #  'test_zernike_moments.py']
 runfile(inpy +'/examples/create_bee_eye.py', wdir = inpy)
+
+#now load a sky
+pip install C:/Users/jaf54iq/src/InvertSy
+#make sure requirements are met
+pip install -r C:/Users/jaf54iq/src/InvertSy/requirements.txt
+# after a pip install of https://github.com/InsectRobotics/InvertSy.git
+#InvertSy not working
+sys.path.append('C:/Users/jaf54iq/src/InvertSy/')
+#I edited invertsy.agent to get it working
+        # runfile('C:/Users/jaf54iq/src/InvertSy/examples/test_sky.py', wdir = 'C:/Users/jaf54iq/src/InvertSy/')
+        # from invertsy.env.sky import Sky
+        # from invertsy.simplot._plots import plot_sky
 
 runfile(inpy + '/examples/test_zernike_moments.py')
 
