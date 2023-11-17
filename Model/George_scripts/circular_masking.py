8a1f15a46ac22ead827404b5c7e58328201e3f96
George Kolyfetis <88230819+GeorgeKolyfetis@users.noreply.github.com>
2023-11-17 15:39:23 +0200

    Add files via upload

--- Model/George_scripts/circular_masking.py
+++ Model/George_scripts/circular_masking.py
@@ -1,17 +1,11 @@
 # usage: python circular_masking.py <infile_path> <desired_radius_in_pixels> <outfile_path>
 # author: Georgios Kolyfetis
 
-from PIL import Image,ImageDraw,ImageOps
+from PIL import Image, ImageDraw
 import sys
 
-
-
-def make_circular(im, radius=None):
-    if radius is None:
-        # Use the default radius if not specified
-        radius = min(im.width, im.height) / 2
-
-    # Create a blank image with the same size as the input image
+def make_circular_mask(im):
+    # Create a circular mask with the same size as the input image
     mask = Image.new("L", im.size, 0)
     draw = ImageDraw.Draw(mask)
 
@@ -19,28 +13,36 @@ def make_circular(im, radius=None):
     center_x = im.width // 2
     center_y = im.height // 2
 
-    # Calculate the bounding box for the circle based on the specified radius
-    box = (center_x - radius, center_y - radius, center_x + radius, center_y + radius)
+    # Calculate the radius as half of the minimum dimension
+    radius = min(center_x, center_y)
 
     # Draw a circular mask within the bounding box
-    draw.ellipse(box, fill=255)
+    draw.ellipse((center_x - radius, center_y - radius, center_x + radius, center_y + radius), fill=255)
 
-    # Create an image with the circular mask
-    out = Image.new("RGBA", im.size)
-    out.paste(im, (0, 0))
+    return mask
+
+def apply_circular_mask(im, mask):
+    # Create a copy of the original image with an alpha channel
+    out = im.convert("RGBA")
+
+    # Apply the circular mask to the alpha channel
     out.putalpha(mask)
 
     return out
 
-
-
-
 if __name__ == "__main__":
+    # Open the input image specified in the command-line arguments
     with Image.open(sys.argv[1]) as im:
-        im = im.convert("RGBA")
-        # Specify the desired radius
-        desired_radius = sys.argv[2]
+        # Ensure the image is square (crop if necessary)
+        size = min(im.size)
+        im = im.crop((0, 0, size, size))
 
-        # Create the circular mask with the specified radius
-        circular_im = make_circular(im, radius=desired_radius)
-        circular_im.save(sys.argv[3])
+        # Create the circular mask
+        circular_mask = make_circular_mask(im)
+
+        # Apply the circular mask to the image
+        result_image = apply_circular_mask(im, circular_mask)
+
+        # Save the resulting image with transparency
+        result_image.save(sys.argv[2])
+
